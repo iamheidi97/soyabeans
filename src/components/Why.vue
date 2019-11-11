f5d524142c33dad8be1559b53a5db4547d6b3311
Heidi Mata <iamheidilouisemata@gmail.com>
2019-11-11 13:51:44 +0800

    11/11

--- src/components/Why.vue
+++ src/components/Why.vue
@@ -5,7 +5,12 @@
     </h3>
     <div class="container-fluid">
         <div class="row">
-          
+          <div class="col-6">
+            Get fast results
+          </div>
+          <div class="col-2">
+            1
+          </div>
         </div>
     </div>
   </div>
