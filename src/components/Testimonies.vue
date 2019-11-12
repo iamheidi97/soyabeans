6aa96bf09217ce9454e837ec5610837a90868c68
Heidi Mata <iamheidilouisemata@gmail.com>
2019-11-12 13:13:19 +0800

    Co-authored-by: uwuxie <rinabelgas@gmail.com>

---
+++ src/components/Testimonies.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3 class="font-weight-bolder my-5">
+            What some of our students had to say
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
